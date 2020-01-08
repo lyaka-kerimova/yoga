d8317f387a75ba42ad72f35e20b2363dc9204265
Asim Salimli <asimsalimli@Asims-MacBook-Pro.local>
2020-01-08 00:19:58 -0500

    lesson about this

--- css/script.js
+++ css/script.js
@@ -39,6 +39,50 @@
      }
 
    });
+
+   //Timer
+
+   let deadline = '2020-01-07' ;
+
+   function getTimeRemaning(endtime) {
+      let t = Date.parse(endtime) - Date.parse(new Date()),
+      seconds = Math.floor((t/1000) % 60),
+      minutes = Math.floor((t/1000/60) % 60),
+      hours = Math.floor((t/(1000*60*60)));
+
+      
+
+      return{
+         'total' : t,
+         'hours' : hours,
+         'minutes' : minutes,
+         'seconds' : seconds
+      };
+   }
+
+   function setClock(id, endtime) {
+      let timer = document.getElementById(id),
+          hours = timer.querySelector('.hours'),
+          minutes = timer.querySelector('.minutes'),
+          seconds = timer.querySelector('.seconds'),
+          timeInterval = setInterval(updateClock, 1000);
+
+      function updateClock() {
+         let t = getTimeRemaning(endtime);
+         hours.textContent = t.hours;
+         minutes.textContent = t.minutes;
+         seconds.textContent = t.seconds;
+         
+
+         if (t.total <=0) {
+            clearInterval(timeInterval);
+         }
+      }
+
+   }
+
+   setClock('timer', deadline);
+
     
 
  });
